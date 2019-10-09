104ed36aed9a88630a9da6a3425bd0dbf7107bce
Nadav Samet <thesamet@gmail.com>
2019-10-09 10:06:45 -0700

    Add sbt cache for travis

--- test_udt_gen.sh
+++ test_udt_gen.sh
@@ -6,4 +6,4 @@ VERSION=$(awk -e '{ print $5 }' version.sbt | tr -d '"')
 sbt +publishLocal
 cp version.sbt test/
 cd test/
-sbt -Dplugin.version=$VERSION test
+sbt -Dplugin.version=$VERSION "${@-test}"
