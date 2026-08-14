6e734935b0ca269cb693b6c8714823ff6046a79f
Frank Yin <franky@ziprecruiter.com>
2026-08-13 17:16:28 -0700

    restructure code to avoid duplicates

---
+++ sparksql-scalapb/src/test/spark-3/scalapb/spark/TestCompatibilityHelpers.scala
@@ -0,0 +1,10 @@
+package scalapb.spark
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+object TestCompatibilityHelpers {
+  def personGetItem(df: DataFrame): DataFrame = {
+    df.select(col("name"), col("addresses").getItem(0))
+  }
+}
